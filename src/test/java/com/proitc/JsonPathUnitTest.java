1c5254bc88d2e5fa168a6085379bc1e9f625ce5c
unknown <earth001@gmail.com>
2018-06-12 17:54:30 -0300

    Added new tests

--- src/test/java/com/proitc/JsonPathUnitTest.java
+++ src/test/java/com/proitc/JsonPathUnitTest.java
@@ -3,9 +3,9 @@ package com.proitc;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.jayway.jsonpath.JsonPath;
-import java.util.HashMap;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 import org.junit.jupiter.api.Test;
 
 class JsonPathUnitTest {
@@ -14,18 +14,27 @@ class JsonPathUnitTest {
   public void givenJsonString_whenCountPropertiesWithJsonPath_thenGetTotal() {
     String jsonString = "{'username':'jhon.user','email':'jhon@company.com','age':'28'}";
 
-    JSONArray jsonArray = JsonPath.parse(jsonString).read("$.*");
+    JSONObject jsonObject = (JSONObject) JSONValue.parse(jsonString);
 
-    assertThat(jsonArray).size().isEqualTo(3);
+    assertThat(jsonObject).size().isEqualTo(3);
   }
 
   @Test
   public void givenJsonString_whenCountPropertiesWithJsonObject_thenGetTotal() {
     String jsonString = "{'username':'jhon.user','email':'jhon@company.com','age':'28'}";
 
-    HashMap jsonObject = JsonPath.parse(jsonString).json();
+    JSONArray jsonArray = JsonPath.read(jsonString, "$.*");
 
-    assertThat(jsonObject.size()).isEqualTo(3);
+    assertThat(jsonArray.size()).isEqualTo(3);
+  }
+
+  @Test
+  public void givenJsonString_whenCountPropertiesWithJsonPath_thenGetSize() {
+    String jsonString = "{'username':'jhon.user','email':'jhon@company.com','age':'28'}";
+
+    JSONArray jsonArray = JsonPath.read(jsonString, "$.*.length()");
+
+    assertThat(jsonArray.size()).isEqualTo(3);
   }
 
 }
